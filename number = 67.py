cad09857e146553b4d93939bad176b93e311d7c8
juicybob123 <okruh2028@kehillahstudent.org>
2026-01-07 23:14:46 -0800

    123

---
+++ number = 67.py
@@ -0,0 +1,16 @@
+number =  3128
+n = 1
+d = number//2
+
+while True:
+    while True:
+        n = n + 1
+        if (number // n) == (number / n):
+                print("not prime")
+        break
+        if (number // n) != (number / n):
+            print("prime")
+            break
+
+    
+    
